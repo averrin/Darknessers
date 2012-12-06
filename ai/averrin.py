26ff8300e3256b072e5afdb7b5fc3694864d2a4e
a.nabrodov <averrin@gmail.com>
2012-12-06 17:23:54 +0400

    averrin is smart guy;)

--- ai/averrin.py
+++ ai/averrin.py
@@ -22,13 +22,13 @@ class Averrin(AI):
                 self.waypoint = QPointF(partner[0].pos.x(), partner[0].pos.y())
                 self.go(partner[0].pos.x(), partner[0].pos.y()).wait()
 
-    # def before_go(self, x, y):
-    #     self.target = self.api.drawPoint(x, y)
-    #     self.path = self.api.drawLine(self.pos.x(), self.pos.y(), x, y)
+    def before_go(self, x, y):
+        self.target = self.api.drawPoint(x, y)
+        self.path = self.api.drawLine(self.pos.x(), self.pos.y(), x, y)
 
-    # def after_go(self, x, y):
-    #     self.target.hide()
-    #     self.path.hide()
+    def after_go(self, x, y):
+        self.target.hide()
+        self.path.hide()
 
     def collision_go(self, x, y):
         self.api.drawPoint(x, y)
